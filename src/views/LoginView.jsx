33d18ccc7eb1e447a2c8fdf5de9f36062c317c1b
Guo <Marcus.Guo@student.tdsb.on.ca>
2025-04-24 13:50:24 -0400

    started working on loginView

--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -1,4 +1,13 @@
 function LoginView() {
+    let [email, setEmail] = useState("");
+    let [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        navigate("/");
+    }
+
     return (
         <div>
             <h1>Login</h1>
